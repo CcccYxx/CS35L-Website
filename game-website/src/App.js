c01077d822456c4f3459a9348bb663ebb658952c
Yuxi Chang <michaelchangyx@g.ucla.edu>
2021-05-13 00:21:22 -0700

    Add initial implementation of NavBar

--- game-website/src/App.js
+++ game-website/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Webpages from './webpages'
 import './App.css';
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Webpages />
     </div>
   );
--- game-website/src/Components/NavBar/NavBar.js
+++ game-website/src/Components/NavBar/NavBar.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { MenuItems } from "./MenuItems";
+import './NavBar.css';
 
-function NavBar() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+class NavBar extends Component{
+    render() {
+        return(
+            <nav className="NavBarItems">
+                <h1 className="app-logo">website-name</h1>
+                <ul>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                   {item.label}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        );
+    };
+};
 
 export default NavBar
