ad089fc298ef0ae13371d5af05f372b10ec410bb
sam717 <samanthalinhhs@gmail.com>
2021-05-16 01:43:33 -0700

    Update News.js

--- game-website/src/webpages/News.js
+++ game-website/src/webpages/News.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './News.css';
 
+function newsArticle(title, author, publisher, link) {
+    this.title = title;
+    this.author = author;
+    this.publisher = publisher;
+    this.link = link;
+}
+var myItems = [
+    new newsArticle('Genshin Impact', 'John Genshinimpact', 'IGN'),
+];
+
+var div = document.createElement('div');
+document.body.appendChild(div);
+
+
 const News = () =>{
     return(
         <div>
             <h1>News</h1>
+            
+            <p>{myItems[0].title} {myItems[0].author} {myItems[0].publisher}</p>
+
         </div>
     );  
 };
