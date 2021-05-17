598630d294e54bea77627d53c16c0566763f67b3
benjamingoldblatt <benjamingoldblatt88@gmail.com>
2021-05-17 12:07:43 -0700

    get profile page started, create branch

--- game-website/src/webpages/Profile.js
+++ game-website/src/webpages/Profile.js
@@ -4,6 +4,13 @@ import './Profile.css';
 const Profile = () =>{
     return(
         <div>
+            <div>
+                <div>
+                    <img style= {{width:"160px",height:"160px",borderRadius:"80px"}}
+                        src="https://images.unsplash.com/photo-1484611941511-3628849e90f7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHBlb3BsZXxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+                    />
+                </div>
+            </div>
             <h1> This is Profile Page </h1>
         </div>
     );  
