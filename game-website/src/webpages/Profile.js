569f0bcdad053d057745b1fbe70615d0c376cff0
benjamingoldblatt <benjamingoldblatt88@gmail.com>
2021-05-17 18:30:39 -0700

    create visual layout, create vert profile divs

--- game-website/src/webpages/Profile.js
+++ game-website/src/webpages/Profile.js
@@ -10,7 +10,7 @@ class Profile extends Component{
             password:'password',
             id:'johnsmith1234',
             friends:'6',
-            interests:'fortnite, chess, strategic games',
+            Games: ['fortnite ', 'chess ', 'strategic games', 'multi-player'],
             image:"https://images.unsplash.com/photo-1484611941511-3628849e90f7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHBlb3BsZXxlbnwwfDJ8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
           };
     }
@@ -20,19 +20,46 @@ class Profile extends Component{
 
     render() {
         return(
-            <div>
-                <div id="Header">
+            <div className='cols'>
+                <div className='top'>
                     <img 
-                        alt='image did not load'
+                        border= '5px solid #555'
+                        alt='did not load'
                         src={this.state.image}
-                        style={{width:"180px", height:"180px", borderRadius:"80px"}}
+                        style={{width:"200px", height:"200px", borderRadius:"80px"}}
                     />
-                    <h1> Name: {this.state.name} </h1>
-                    <h1> UserId: {this.state.id} </h1>
-                    <h1> Friends: {this.state.friends} </h1>
-                    <h1> Interests: {this.state.interests} </h1>
-                    <input type='text' value = {this.state.image} onChange={this.handleChange}/>
-
+                    <div className='usersummary'> 
+                        <h1> Profile</h1>
+                        <ul>
+                            <li> Name - {this.state.name} </li>
+                            <li> UserId - {this.state.id} </li>
+                            <li> Email - {this.state.email} </li>
+                            <li> Friends - {this.state.friends} </li>
+                        </ul>
+                    </div>
+                    <div className='Games'> 
+                        <h1> Top Games</h1>
+                        <ul>
+                            <li> {this.state.Games[0]}</li>
+                            <li> {this.state.Games[1]}</li>
+                            <li> {this.state.Games[2]}</li>
+                            <li> {this.state.Games[3]}</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <form>
+                            <label>
+                                change url:
+                                <input type='text' value = {this.state.image} onChange={this.handleChange}/>
+                            </label>
+                        </form>
+                    </div>
+                </div>
+                <div className='blogs'>
+                    <h1> Blogs</h1>
+                </div>
+                <div className='chats'>
+                    <h1> Friends</h1>
                 </div>
             </div>
         );  
