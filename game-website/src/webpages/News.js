cbea5e0c04cb0ed92d56fc53c1dcbb00316d82c6
sam717 <samanthalinhhs@gmail.com>
2021-05-21 21:02:33 -0700

    added the news json

--- game-website/src/webpages/News.js
+++ game-website/src/webpages/News.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './News.css';
 
-function newsArticle(title, author, publisher, link) {
+function newsArticle(title, author, publisher, link, date) {
     this.title = title;
     this.author = author;
     this.publisher = publisher;
     this.link = link;
+    this.date = date;
 }
-var myItems = [
-    new newsArticle('Genshin Impact', 'John Genshinimpact', 'IGN'),
-];
 
-var div = document.createElement('div');
-document.body.appendChild(div);
+var myItems = [];
+
+
+//for(int i = 0, i < 5, i++){
+//    var div = document.createElement('div');
+//    document.body.appendChild(div);
+//}
+
 
 
 const News = () =>{
