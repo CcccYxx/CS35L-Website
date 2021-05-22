42cd246ded58bd50eb09e6184ad19f4539d57876
sam717 <samanthalinhhs@gmail.com>
2021-05-22 00:05:25 -0700

    parsed the json into an array to be used in the js

--- game-website/src/webpages/News.js
+++ game-website/src/webpages/News.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './News.css';
 
+const newsJson = require("../assets/news.json");
 function newsArticle(title, author, publisher, link, date) {
     this.title = title;
     this.author = author;
     this.publisher = publisher;
-    this.link = link;
     this.date = date;
+    this.link = link;
 }
 
 var myItems = [];
+var key;
+var item;
+for( key in newsJson){
+    var pushArt = new newsArticle(newsJson[key][0], newsJson[key][1], newsJson[key][2], newsJson[key][3], newsJson[key][4]);
+    myItems.push(pushArt);
+}
 
 
-//for(int i = 0, i < 5, i++){
-//    var div = document.createElement('div');
-//    document.body.appendChild(div);
-//}
+for(var item in myItems){
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+}
 
 
 
@@ -24,7 +31,7 @@ const News = () =>{
         <div>
             <h1>News</h1>
             
-            <p>{myItems[0].title} {myItems[0].author} {myItems[0].publisher}</p>
+           <p>{myItems[0].title} {myItems[0].author} {myItems[0].publisher}</p>
 
         </div>
     );  
