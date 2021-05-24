2cbc30e1cbecafe0f4b3534237558c3fc08c60ad
sam717 <samanthalinhhs@gmail.com>
2021-05-24 14:49:51 -0700

    Now displays news titles twice

--- game-website/src/webpages/News.js
+++ game-website/src/webpages/News.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './News.css';
+
 
 const newsJson = require("../assets/news.json");
 function newsArticle(title, author, publisher, link, date) {
@@ -10,32 +12,49 @@ function newsArticle(title, author, publisher, link, date) {
     this.link = link;
 }
 
+class newsPage extends React.Component{
+    constructor(){
+        super();
+            this.state ={
+                content: "Display"
+            }
+    }
+
+
+}
+
 function parseJson(){
     var myItems = [];
     var key;
-    var item;
-    for( key in newsJson){
+    for(key in newsJson){
         var pushArt = new newsArticle(newsJson[key][0], newsJson[key][1], newsJson[key][2], newsJson[key][3], newsJson[key][4]);
         myItems.push(pushArt);
     }
 
+    var newsDiv = document.createElement('div');
+    newsDiv.id = "newsList";
+    document.body.appendChild(newsDiv);
     var toAdd = document.createDocumentFragment();
-    for(var item in myItems){
+    
+    for(var i = 0; i < myItems.length;  i++){
+        
         var newDiv = document.createElement('div');
-        newDiv.id = 'art'+item;
+        newDiv.id = 'art'+i;
         newDiv.className = 'articles';
+        newDiv.innerHTML = myItems[i].title;
         toAdd.appendChild(newDiv);
     }
-    document.getElementById("newsList").appendChild(toAdd);
+    alert("ONCE");
+    newsDiv.appendChild(toAdd);
 }
 
 const News = () =>{
     return(
         <div>
             <h1>News</h1>
-            <div id = "newsList">
-                {parseJson}
-            </div>
+                <newsPage>
+                    {parseJson()}
+                </newsPage>
         </div>
     );  
 };
