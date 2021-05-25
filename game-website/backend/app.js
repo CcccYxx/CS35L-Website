ac6d395ba0ef18c209b6a1aa6a90b79196a8b887
benjamingoldblatt <benjamingoldblatt88@gmail.com>
2021-05-25 10:46:03 -0700

    cleaned up backend files

--- game-website/backend/app.js
+++ game-website/backend/app.js
@@ -3,17 +3,11 @@ const mongoose = require("mongoose");
 const app = express();
 const userRoute = require('./user-route.js')
 const PORT = 8080;
+const cors = require("cors");
 require('dotenv').config()
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.DB_CONNECTION_STRING;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+//Middlewares
+app.use(cors());
 //bodyParser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,6 +19,8 @@ app.get("/", (req, res) => {
 
 mongoose.connect(
     process.env.DB_CONNECTION_STRING,
+    { useUnifiedTopology: true },
+    { useNewUrlParser: true },
     (req,res) => {
     console.log("Connected to the database");
 });
