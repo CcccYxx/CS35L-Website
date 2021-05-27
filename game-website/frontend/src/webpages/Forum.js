a7b5cfacebc83cc876f20904fb6831dce50f7a50
QxLyds <gonglydia123@gmail.com>
2021-05-26 18:33:35 -0700

    Add Forum Page

--- game-website/frontend/src/webpages/Forum.js
+++ game-website/frontend/src/webpages/Forum.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Container } from '@material-ui/core';
+import { Grow, Grid } from '@material-ui/core';
+import Posts from '../Components/Uploads/Posts.js'
+import Form from '../Components/Form/form'
 
 const Forum = () => {
     return(
-        <h1>Discussion/Forum page</h1>
+        <Container> 
+           <Grow in> 
+                <Container>
+                    <Grid container justify= "space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                         </Grid>   
+                         <Grid item xs={12} sm={4}>
+                             <Form />
+                         </Grid>
+                    </Grid>
+                </Container>
+           </Grow>
+        </Container>
     );
 };
 
