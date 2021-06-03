73e23f90953a64fb0d140edc02b09a494e1c4b67
Yuxi Chang <michaelchangyx@g.ucla.edu>
2021-06-02 18:40:55 -0700

    Now can display posts search results

--- game-website/frontend/src/webpages/Home.js
+++ game-website/frontend/src/webpages/Home.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import './Home.css';
-
+import Post from '../Components/Post/Post'
 
 class Home extends React.Component{
     constructor(props){
@@ -24,9 +24,9 @@ class Home extends React.Component{
         if(event.charCode === 13 && event.target.value){
             axios.get('/search/user/' + this.state.searchString)
             .then((res) => {
-                if(res.status === 200){
+                if(res.status === 200 || res.status === 204){
                     this.setState({
-                        users: res.data,
+                        users: res.data? res.data.reverse() : [],
                         displaySearchResult: true
                     })
                 }else if(res.status === 404){
@@ -35,28 +35,52 @@ class Home extends React.Component{
             })
             axios.get('/search/post/' + this.state.searchString)
             .then((res) => {
-                if(res.status === 200){
+                if(res.status === 200 || res.status === 204){
                     this.setState({
-                        posts: res.data,
+                        posts: res.data? res.data.reverse() : [],
                         displaySearchResult: true
                     })
+                    console.log(this.state.users)
+                    console.log(this.state.posts)
                 }else if(res.status === 404){
                     alert('Error when searching')
                 }
+            })
+        }else if(event.charCode === 13){
+            this.setState({
+                posts: [],
+                users: [],
+                displaySearchResult: false
             })
         }
     }
 
     render(){
+        var postsCount = this.state.posts.length
         return(
-            <div className='welcome'>
-                <div className="homePageContainer">
-                    <div className={this.state.displaySearchResult ? 'hide' : 'welcome-msg'}> Welcome to Game-Website </div>
-                    <div className="searchbar_container">
-                        <input type="text" className="searchbar" placeholder="Search for posts or people..." maxLength="40"
-                        onChange={this.handleChange.bind(this)} onKeyPress={this.handleSearchStringChange.bind(this)}/>
-                    </div>
-
+            <div className={this.state.displaySearchResult ? 'welcomeSearch' : 'welcome'}>
+                <div className={this.state.displaySearchResult ? 'hide' : 'welcome-msg'}> Welcome to Game-Website </div>
+                <div className="searchbar_container">
+                    <input type="text" className="searchbar" placeholder="Search for posts or people..." maxLength="40"
+                    onChange={this.handleChange.bind(this)} onKeyPress={this.handleSearchStringChange.bind(this)}/>
+                </div>
+                <div className="searchResultContainer">
+                <div className="posts">
+                    {this.state.posts.slice(0, postsCount % 2 === 0 ? postsCount/2 : postsCount/2 + 1).map((post, index) => {
+                        return(<Post key={post._id} title={post.title} message = {post.message} tags={post.tags} creator={post.creator} 
+                            date={post.createdAt} likeCount={post.likeCount} selectedFile={post.selectedFile} 
+                            onClickLike={null} onClickDel={null} 
+                            i={index}/>)
+                    })}   
+                </div>
+                <div className="posts">
+                    {this.state.posts.slice(postsCount % 2 === 0 ? postsCount/2 : postsCount/2 + 1, postsCount).map((post, index) => {
+                        return(<Post key={post._id} title={post.title} message = {post.message} tags={post.tags} creator={post.creator} 
+                            date={post.createdAt} likeCount={post.likeCount} selectedFile={post.selectedFile} 
+                            onClickLike={null} onClickDel={null} 
+                            i={index +  (postsCount % 2 === 0 ? Math.floor(postsCount/2) : Math.floor(postsCount/2 + 1))}/>)
+                    })}   
+                </div>
                 </div>
             </div>
         );  
