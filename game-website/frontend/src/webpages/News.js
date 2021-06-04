04de14916c54d4522c957fed0456239bef44ea78
Thundersquirrels <carson@reality.net>
2021-06-03 20:02:53 -0700

    Reverted News.js to working version for demo.

--- game-website/frontend/src/webpages/News.js
+++ game-website/frontend/src/webpages/News.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './News.css';
-import {Link} from 'react-router-dom';
-const axios = require('axios');
-
-function newsArticle(key, title, author, publisher, date, link) {
-    this.id = key;
+import {Link} from 'react-router-dom'
+const newsJson = require("../assets/news.json");
+function newsArticle(title, author, publisher, date, link) {
     this.title = title;
     this.author = author;
     this.publisher = publisher;
@@ -12,27 +10,12 @@ function newsArticle(key, title, author, publisher, date, link) {
     this.link = link;
 }
 
-
-async function getNews(){
-    const response = await axios.get('http://localhost:8080/news_api');
-    const data = response.data;
-    return data;
-};
-
-var news_json;
 var myItems = [];
 var key;
-async function get_json(){
-    news_json = await getNews();
-    for (key in news_json.articles) {
-        console.log(news_json.articles[key]);
-        myItems.push(new newsArticle(news_json.articles[key], news_json.articles[key].title, news_json.articles[key].author, news_json.articles[key].source.name, news_json.articles[key].publishedAt, news_json.articles[key].url));
-    }
+for( key in newsJson){
+    var pushArt = new newsArticle(newsJson[key][0], newsJson[key][1], newsJson[key][2], newsJson[key][3], newsJson[key][4]);
+    myItems.push(pushArt);
 }
-//news_json = getNews();
-get_json();
-//alert(Json.parse(news_json));
-
 
 class News extends React.Component{
     constructor(props){
@@ -44,7 +27,7 @@ class News extends React.Component{
         const news = myItems.map((items) => {
             return( //return list for now, can change those if needed 
                 <ul className="newlist">
-                    <li className="news-block" key={items.id} >
+                    <li className="news-block" key={items.title} >
                         <h2 className="news-title"><Link to={{pathname:items.link}} target="_blank" className="news-link">{items.title}</Link></h2>
                         <p className="author-publisher">{items.author}, {items.publisher}</p>
                     </li>
